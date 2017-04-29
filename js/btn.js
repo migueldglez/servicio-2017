0cddfff65782424e1d5574986fc5a2265e69d259
DarkSylar117 <t.anto.j@gmail.com>
2017-04-29 17:28:22 -0500

    script de eventos de los botones

--- js/btn.js
+++ js/btn.js
@@ -1,12 +1,41 @@
+const res = require('../js/resolucion.js');
+
 var _siguiente =  document.getElementById('siguiente')
 var _tips = document.getElementById('tips')
 var _enviar = document.getElementById('enviar')
+var _valor = document.getElementById('respuestas')
 
+res.terminos.errs = 0
 
 function mostrar() {
+  console.log(_valor.value);
+  if(_valor.value==0 && !_valor.value==''){
+    res.pushEcuacion()
+    _valor.value = ''
+    res.terminos.errs = 0
+    _tips.classList.add('hide')
+    _siguiente.classList.add('hide')
+  }else{
+    res.terminos.errs = (res.terminos.errs +1)
+    if(res.terminos.errs===3){
+      _tips.classList.remove('hide')
+    }else if(res.terminos.errs===5){
+      _siguiente.classList.remove('hide')
+    }
+  }
 
-  _siguiente.classList.remove('hide')
-  _tips.classList.remove('hide')
+
+
+
 }
 
+window.onload = () => {
+  res.pushEcuacion();
+  document.getElementById('igual').innerHTML = "="
+}
+
+document.getElementById('siguiente').onclick = res.pushEcuacion;
+
 _enviar.onclick = mostrar
+
+exports.t = res.terminos
