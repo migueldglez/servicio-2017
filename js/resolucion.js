d833c65a097bfa131ee57007432a32c4f1637293
DarkSylar117 <t.anto.j@gmail.com>
2017-04-29 14:32:39 -0500

    muestra el resultado de una ecuacion aleatoria

--- js/resolucion.js
+++ js/resolucion.js
@@ -1,9 +1,48 @@
 const ecuacion = require('../js/funciones.js');
 
+var _terminos = {}
+
+function newEcuacion(termino) {
+  var ax1 = termino.m1.split(' ')
+  var ax2 = termino.m2.split(' ')
+
+  var a1 = splitEcua(ax1)
+  var a2 = splitEcua(ax2)
+
+  _terminos.x = (a1[0]-a2[0])
+  _terminos.c = (a2[1]-a1[1])
+  console.log('x = '+ _terminos.x + '/' +_terminos.c);
+}
+
+function splitEcua(array) {
+  var a = [0,0]
+
+  for (var i = 0; i < array.length; i++) {
+    if(array[i].includes("x")){
+      switch (array[i]) {
+        case 'x':
+          a[0]++
+          break;
+        case '-x':
+          a[0]--
+          break;
+        default:
+          a[0] +=  parseInt(array[i].replace("x",""))
+      }
+    }else{
+      a[1] += parseInt(array[i])
+    }
+  }
+
+  return a
+}
+
+
 function pushEcuacion() {
   var n = ecuacion.ecuacion()
-  localStorage.setItem("m1",n[0]);
-  localStorage.setItem("m2",n[1]);
+  _terminos.m1 = n[0]
+  _terminos.m2 = n[1]
+  newEcuacion(_terminos)
   document.getElementById('miembro1').innerHTML = n[0]
   document.getElementById('miembro2').innerHTML = n[1]
 }
@@ -14,3 +53,5 @@ window.onload = () => {
 }
 
 document.getElementById('siguiente').onclick = pushEcuacion;
+
+exports.terminos = _terminos;
