a1727921fe5d2b979d544bdc864b31fb3960b9a3
DarkSylar117 <t.anto.j@gmail.com>
2017-05-01 15:11:20 -0500

    Script de eventos de los botones y efectos de sonido

--- js/btn.js
+++ js/btn.js
@@ -5,15 +5,22 @@ var _siguiente =  document.getElementById('siguiente')
 var _tips = document.getElementById('tips')
 var _enviar = document.getElementById('enviar')
 var _valor = document.getElementById('respuestas')
+var _resueltas = document.getElementById('resueltas')
+var _score = document.getElementById('score')
+var _intentos = document.getElementById('intentos')
+var _icono = document.getElementById('bien')
+var _efecto = document.getElementById('efecto')
 
 res.terminos.errs = 0
 res.terminos.goods = 0
 res.terminos.global = 0
+res.terminos.score = 0
 
 //mustra los botones siguiente y tips de acuerdo al numero
 //de errores
 function mostrar() {
   if(_valor.value==0 && !_valor.value==''){
+    iconAnimation(1)
     res.pushEcuacion()
     _valor.value = ''
     res.terminos.errs = 0
@@ -21,6 +28,9 @@ function mostrar() {
     _siguiente.classList.add('hide')
     res.terminos.goods++
     res.terminos.global++
+    res.terminos.score+=100
+    _resueltas.innerHTML = res.terminos.global
+    _score.innerHTML = res.terminos.score + ' Exp'
     switch (res.terminos.goods) {
       case 1:
         puzzle._ban.intent = 1
@@ -34,8 +44,11 @@ function mostrar() {
       default:
         puzzle._ban.intent = 4
     }
-
+    _intentos.innerHTML = puzzle._ban.intent
   }else{
+    iconAnimation(0)
+    if(res.terminos.score>0)res.terminos.score-=50
+    _score.innerHTML = res.terminos.score + ' Exp'
     res.terminos.errs = (res.terminos.errs +1)
     if(res.terminos.errs===3){
       _tips.classList.remove('hide')
@@ -62,6 +75,35 @@ function enter(e) {
     mostrar()
     return false
   }
+}
+
+//Animacion y efecto de sonidos de los iconos al cometer errores o al acertar
+function iconAnimation(num) {
+  var icono = (num==1) ? 'done' : 'not_interested'
+  var bien = ['green-text', 'text-darken-4']
+  var mal = ['red-text', 'text-darken-4']
+  var clases = (num==1) ? bien : mal
+  var a_src = (num==1) ? '../effects/Correct-answer.mp3' : '../effects/Wrong-answer-sound-effect.mp3'
+
+
+  _icono.innerHTML = icono
+  for (i of clases) {
+    _icono.classList.add(i)
+  }
+  _efecto.src = a_src
+  _efecto.volume = .3
+  _efecto.play()
+
+  _icono.classList.remove('hide')
+
+  setTimeout(function () {
+    _icono.classList.add('hide')
+    for (i of clases) {
+      _icono.classList.remove(i)
+    }
+  }, 1000);
+
+
 }
 
 //Al cargas la pagina game.html lanza la funcion pushEcuacion()
