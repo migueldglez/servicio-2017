ac592adddd18964d127f519dddae997b6f1a9bdf
DarkSylar117 <t.anto.j@gmail.com>
2017-05-05 12:09:47 -0500

    Soluciona una ecuacion aleatoria

--- js/resolucion.js
+++ js/resolucion.js
@@ -1,18 +1,25 @@
 const ecuacion = require('../js/funciones.js');
 
-var _terminos = {}
+var _terminos = {canM1:0,canM2:0}
 
-function newEcuacion(termino) {
+
+//Suma la cantidad de terminos independientes y dependientes de ambos mienbros de una
+//funcion
+function respuesta(termino) {
   var ax1 = termino.m1.split(' ')
   var ax2 = termino.m2.split(' ')
 
   var a1 = splitEcua(ax1)
   var a2 = splitEcua(ax2)
-
+  _terminos.m1X = a1[0]
+  _terminos.m1I = a1[1]
+  _terminos.m2X = a2[0]
+  _terminos.m2I = a2[1]
   _terminos.x = (a1[0]-a2[0])
   _terminos.c = (a2[1]-a1[1])
 }
 
+//Devuelve un arreglo con la suma de las x en un termino, y los terminos independientes
 function splitEcua(array) {
   var a = [0,0]
 
@@ -41,10 +48,14 @@ function splitEcua(array) {
 
 
 exports.pushEcuacion = () => {
-  var n = ecuacion.ecuacion()
+  var c1 = ecuacion.cantTerminos()
+  var c2 =ecuacion.cantTerminos()
+  var r1 = (_terminos.canM1===0)? c1:_terminos.canM1
+  var r2 = (_terminos.canM2===0)? c2:_terminos.canM2
+  var n = ecuacion.ecuacion(r1,r2)
   _terminos.m1 = n[0]
   _terminos.m2 = n[1]
-  newEcuacion(_terminos)
+  respuesta(_terminos)
   document.getElementById('miembro1').innerHTML = n[0]
   document.getElementById('miembro2').innerHTML = n[1]
 }
