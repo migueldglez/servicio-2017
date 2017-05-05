e89e9d6c16f1b8071162ad472ee811c2c6a4a713
DarkSylar117 <t.anto.j@gmail.com>
2017-05-05 12:07:01 -0500

    Inicia los eventos de los scripts de materialize-css

--- js/init.js
+++ js/init.js
@@ -1,4 +1,22 @@
+const btn = require('../js/btn.js');
+const proceso = require('../js/procSolucion');
+
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('#tipsM').modal();
+    var _closeV = document.getElementById('closeTM')
+    var _video = document.getElementById('vTutorial')
+
+
+    $('#tipsM').modal({
+      complete: function () { _video.pause() },
+      ready: function () { setTimeout(function () {
+        _video.play()
+      }, 1000);}
+    });
+
+
+    $('#solucionM').modal({
+      ready: function () {proceso.procSol()},
+      complete: function () {btn.ocultar()}
+    });
   });
