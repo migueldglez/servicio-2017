cee81b2bd75c54c63145a8e000bdb602f93be24c
DarkSylar117 <t.anto.j@gmail.com>
2017-06-11 00:41:34 -0500

    Add: Script inicializadore del index.html

---
+++ js/intro-init.js
@@ -0,0 +1,49 @@
+const {ipcRenderer} = require('electron')
+const remote = require('electron').remote
+
+const path = require('path')
+const url = require('url')
+
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+  $('.scrollspy').scrollSpy({scrollOffset:0});
+
+  $('#registro').modal({
+    dismissible:false,
+    endingTop: '15%',
+    outDuration: 300,
+    complete: function () {
+      Materialize.showStaggeredList('#menuButtons');
+      setTimeout(function () {
+        $('#menuButtons').removeClass('hide')
+      },100)
+    }
+  });
+
+  $('#iniciar').on('click',function () {
+    setTimeout(function () {
+      $('#registro').modal('open');
+    },355)
+  });
+
+  $('#subSesion').on('click',function () {
+    $('#registro').modal('close');
+  });
+
+  $('#iniGame').on('click',function () {
+    var linkUrl = url.format({
+        pathname: path.join(__dirname,'../templates/main.html'),
+        protocol: 'file:',
+        slashes: true
+      })
+
+      ipcRenderer.send('load-page', linkUrl)
+  })
+
+  $('#closeGame').on('click',function () {
+    var win = remote.getCurrentWindow()
+    win.close()
+  })
+
+});
