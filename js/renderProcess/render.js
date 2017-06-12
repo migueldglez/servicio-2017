60498e1459a2ea3353804ba4d22315dc31dd055e
DarkSylar117 <t.anto.j@gmail.com>
2017-06-12 13:00:27 -0500

    Remove: Variables y funciones inutilizables

--- js/renderProcess/render.js
+++ js/renderProcess/render.js
@@ -4,29 +4,21 @@ const remote = require('electron').remote
 const path = require('path')
 const url = require('url')
 
-const change = document.getElementById('change')
 const closeBrows = document.getElementById('closeBrows')
 const recargar = document.getElementById('reload')
 const mini = document.getElementById('minimizar')
-
-var linkUrl = url.format({
-    pathname: path.join(__dirname, 'game.html'),
-    protocol: 'file:',
-    slashes: true
-  })
 
 //Cierra la ventana  actual
 function cerrar() {
   var win = remote.getCurrentWindow()
   win.close()
 }
-
 //Recarga la ventana actual
 function reload() {
   var win = remote.getCurrentWindow()
   win.reload()
 }
-
+//Minimizar la ventana actual
 function minimizar() {
   var win = remote.getCurrentWindow()
   win.minimize();
