68beb8422ecb25a9033a0fbd92758d1a984564d8
DarkSylar117 <t.anto.j@gmail.com>
2017-06-18 20:22:12 -0500

    Add: Modulo de comprobacion de archivos de configuracion

---
+++ modulos/setSettings.js
@@ -0,0 +1,70 @@
+const storage = require('electron-storage');
+const jstorage = require('electron-json-storage');
+const path = require('path')
+const fs = require('fs');
+
+
+/**
+ * @summary Set file settings.json
+ * @function
+ * @private
+ *
+ * @description
+ * Copia un archivo json por deafult, y lo copia en un objeto javascript
+ *
+ * @param {obj} objeto javascript
+*/
+function setFileJSON() {
+  fs.readFile(path.join(__dirname,'files/settingsDefault.json'), 'utf8', function (err, data) {
+    if (err){
+      console.log('No se encontro archivo');
+      throw err
+    } else {
+      var obj = JSON.parse(data);
+      creatJSON(obj)
+    }
+  });
+}
+
+/**
+ * @summary Creat file settings.json
+ * @function
+ * @private
+ *
+ * @description
+ * Crea el directorio userData y dentro el JSON 'settings.json'
+ *
+ * @param {obj} objeto javascript
+ **/
+function creatJSON(obj) {
+  storage.set('userData/settings', obj)
+  .then(() => {
+    console.log('The file was successfully written to the storage');
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
+
+/**
+ * @summary Check settings.json
+ * @function
+ * @public
+ *
+ * @description
+ * Verifica que el archivo 'settings.json' y el fichero 'userData' existe en el
+ * directorio de datos del sistema de la aplicacion, si no existe
+ * crea el directorio userData y dentro el JSON 'settings.json'
+ *
+ * @param {obj} objeto javascript
+ **/
+module.exports = ()=>{
+  storage.isPathExists('userData/settings.json', (itDoes) => {
+    if (itDoes) {
+      console.log('pathDoesExists !')
+    }else {
+      console.log('pathDoesNotExists !');
+      setFileJSON()
+    }
+  });
+};
