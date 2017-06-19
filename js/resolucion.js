5e1f0e5eebb8c0358b3fe7f55f7a11f64257b83c
DarkSylar117 <t.anto.j@gmail.com>
2017-06-19 18:21:07 -0500

    Change: Objeto atributo, funciones, modulos exportados

--- js/resolucion.js
+++ js/resolucion.js
@@ -1,13 +1,22 @@
 const ecuacion = require('../js/funciones.js');
 
-var _terminos = {canM1:0,canM2:0}
-
+var _terminos = {
+  errors : 0,
+  correct : 0,
+  global : 0,
+  score : 0,
+  m1X : 0,
+  m1I : 0,
+  m2X : 0,
+  x : 0,
+  c: 0
+}
 
 //Suma la cantidad de terminos independientes y dependientes de ambos mienbros de una
 //funcion
-function respuesta(termino) {
-  var ax1 = termino.m1.split(' ')
-  var ax2 = termino.m2.split(' ')
+function respuesta() {
+  var ax1 = _terminos.m1.split(' ')
+  var ax2 = _terminos.m2.split(' ')
 
   var a1 = splitEcua(ax1)
   var a2 = splitEcua(ax2)
@@ -47,17 +56,27 @@ function splitEcua(array) {
 }
 
 
-exports.pushEcuacion = () => {
-  var c1 = ecuacion.cantTerminos()
-  var c2 =ecuacion.cantTerminos()
-  var r1 = (_terminos.canM1===0)? c1:_terminos.canM1
-  var r2 = (_terminos.canM2===0)? c2:_terminos.canM2
-  var n = ecuacion.ecuacion(r1,r2)
+exports.setEcuacion = function () {
+  var n = ecuacion.ecuacion()
   _terminos.m1 = n[0]
   _terminos.m2 = n[1]
-  respuesta(_terminos)
+  respuesta()
   var cadena = n[0]+' '+' = '+' '+n[1]
-  document.getElementById('ecuaMath').innerHTML = cadena
+  $('#ecuaMath').text(cadena)
 }
 
-exports.terminos = _terminos;
+
+exports.getData = function (index) {
+  return _terminos[index]
+}
+exports.setData = function (index,data) {
+  _terminos[index] = data
+}
+exports.modData = function (index,data) {
+  if (data>0) {
+    _terminos[index]+=data
+  }else{
+    _terminos[index]-=data
+  }
+
+}
